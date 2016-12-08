b70c05405dc43c542cf1cb28ba5f22f40049ee40
Campbell, Chanse (JavaScript Developer) <chanse.campbell@oddschecker.com>
2016-12-08 20:41:47 +0000

    Testing canvas shape rendering

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const canvas = document.getElementById("myCanvas");
 const ctx    = canvas.getContext("2d");
 
+// Solid rectangle
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
+ctx.closePath();
+
+// Solid circle
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+// Outlined rectangle
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
 ctx.closePath();
