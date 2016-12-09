6a9e8d168049ee34b698b462360636e21d791a01
Campbell, Chanse (JavaScript Developer) <chanse.campbell@oddschecker.com>
2016-12-09 23:56:54 +0000

    Renamed the game as I got it wrong. Woops.

--- app.js
+++ app.js
@@ -38,10 +38,12 @@ var rightPressed = false;
 var leftPressed = false;
 
 var score = 0;
+var lives = 3;
 
 // event listeners
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 // event changing variables
 function keyDownHandler(e) {
@@ -57,6 +59,13 @@ function keyUpHandler(e) {
         rightPressed = false;
     } else if(e.keyCode == 37) {
         leftPressed = false;
+    }
+}
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
     }
 }
 
@@ -84,6 +93,12 @@ function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Score: " +score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: " +lives, canvas.width-65, 20);
 }
 
 // creation of the bricks
@@ -130,6 +145,7 @@ function draw() {
     drawBall();
     drawPaddle();
     drawScore();
+    drawLives();
     collisionDetection();
 
     // paddle movement
@@ -153,18 +169,28 @@ function draw() {
         if(x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         } else {
-            alert("GAME OVER!");
-            document.location.reload();
+            lives--;
+            if(!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width/2;
+                y = canvas.height-30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
         }
     }
 
     x += dx;
     y += dy;
-  
+    
+    requestAnimationFrame(draw);
 }
 
-setInterval(draw, 10);
-
+draw();
 
 
 
