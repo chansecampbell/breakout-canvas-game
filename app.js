47e9a508307d1f06d5261b4b4b523cdbe7ad874c
Campbell, Chanse (JavaScript Developer) <chanse.campbell@oddschecker.com>
2016-12-09 20:26:58 +0000

    Game logic now works, Paddle can successfully hit the ball or cause a game over

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+// render the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx    = canvas.getContext("2d");
 
+// define the movements
 var x      = canvas.width / 2;
 var y      = canvas.height - 30;
 var dx     = 2;
 var dy     = -2;
 
+// ball size
 var ballRadius = 10;
 
+// paddle size
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+// key press
+var rightPressed = false;
+var leftPressed = false;
+
+// event listeners
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// event changing variables
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    } else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    } else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+// creation of the ball
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -16,16 +50,45 @@ function drawBall() {
     ctx.closePath();
 }
 
+// creation of the paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// the movement/recreation
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
+    // paddle movement
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    // collision logic
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        
+        // check for paddle collision if the bottom wall is hit
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER!");
+            document.location.reload();
+        }
     }
 
     x += dx;
@@ -34,3 +97,11 @@ function draw() {
 }
 
 setInterval(draw, 10);
+
+
+
+
+
+
+
+
