f6f7980739d98300aa60f5e516ad8538b8e032cb
Campbell, Chanse (JavaScript Developer) <chanse.campbell@oddschecker.com>
2016-12-12 23:22:41 +0000

    Boundary logic has been readded but it is slightly off right now

--- new.js
+++ new.js
@@ -2,18 +2,29 @@
 
 class Game {
     constructor() {
-        this.canvas  = document.getElementById("myCanvas");
-        this.ctx     = this.canvas.getContext('2d');
-        this.width   = this.canvas.width / 2;
-        this.height  = this.canvas.height - 30;
+        this.width   = canvas.width / 2;
+        this.height  = canvas.height - 30;
         this.dWidth  = 2;
         this.dHeight = -2;
         this.ball    = new Ball();
     }
 
+    boundaryLogic() {
+        if(this.width + this.dWidth > canvas.width-10 || this.width + this.dWidth < 10) {
+            this.dWidth = -this.dWidth;
+            console.log("Ah");
+        }
+
+        if(this.height + this.dHeight < 10 || this.height + this.dHeight > canvas.height-10) {
+            this.dHeight = -this.dHeight;
+            console.log("Oh");
+        }  
+    }
+
     draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.ball.drawBall(this.width, this.height);
+        this.boundaryLogic();
         this.width += this.dWidth;
         this.height += this.dHeight;
 
@@ -24,19 +35,19 @@ class Game {
 class Ball {
     constructor() {
         this.ballRadius = 10;
-        this.canvas  = document.getElementById("myCanvas");
-        this.ctx     = this.canvas.getContext('2d');
     }
 
     drawBall(height, width) {
-        this.ctx.beginPath();
-        this.ctx.arc(width, height, this.ballRadius, 0, Math.PI*2);
-        this.ctx.fillStyle = "#0095DD";
-        this.ctx.fill();
-        this.ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(width, height, this.ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
     }
 }
 
+const canvas = document.getElementById("myCanvas");
+const ctx    = canvas.getContext('2d');
 new Game().draw();
 // 'use strict';
 // class Ball {
